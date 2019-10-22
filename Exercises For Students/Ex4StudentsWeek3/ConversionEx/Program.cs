c27ed39c35b51baabacbb785f698da066bc29a0b
tatyana andreeva <tatyana144@abv.bg>
2019-10-22 21:42:12 +0300

    Added two exaample and a task

---
+++ Exercises For Students/Ex4StudentsWeek3/ConversionEx/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConversionEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i;
+
+            int[] a = new int[10];
+
+            Console.Write("Enter number to convert ");
+            n = int.Parse(Console.ReadLine());
+
+            for( i = 0; 0 < n; i++)
+            {
+                a[i] = n % 2;
+                n = n / 2;
+            }
+            Console.Write("Binary of the given number= ");
+            for(i= i-1; i >=0; i--)
+            {
+                Console.Write(a[i]);
+            }
+        }
+    }
+}
---
+++ Exercises For Students/Ex4StudentsWeek5/Ex4Students4FirstTest/Conversion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number to convert ");
+            Conversion();
+        }
+        static void Conversion()
+        {
+            int n, i;
+
+            int[] a = new int[10];
+
+            n = int.Parse(Console.ReadLine());
+
+            for (i = 0; 0 < n; i++)
+            {
+                a[i] = n % 2;
+                n = n / 2;
+            }
+            Console.Write("Binary of the given number= ");
+            for (i = i - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]);
+            }
+        }
+    }
+}
